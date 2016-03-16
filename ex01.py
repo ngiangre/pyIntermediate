cc697103fbd4c884dd228d2bcbf8026d3321941c
Alex <codenih@gmail.com>
2016-03-15 21:35:56 -0400

    New exercises

---
+++ ex01.py
@@ -0,0 +1,4 @@
+'''
+Exercise 1
+Objective: create a function fibonacci(n) that will return the fibonacci sequence to the nth place
+'''
---
+++ ex02.py
@@ -0,0 +1,5 @@
+'''
+Exercise 2
+Objective: Create a python bot to download all pictures from a provided URL into a folder called: NatGeoPix
+URL: http://proof.nationalgeographic.com/2016/02/02/photo-of-the-day-best-of-january-3/
+'''
